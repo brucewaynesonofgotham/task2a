1e7d9058cddfadeab8b7a2cd871663ae243fa787
brucewayne <brucewaynesonofgotham@gmail.com>
2016-11-08 02:30:51 +0300

    add task2a

--- src/index.js
+++ src/index.js
@@ -12,3 +12,8 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
+
+app.get('/task2a', (req,res) => {
+  const sum = (+ req.query.a || 0) + (+ req.query.b || 0);
+res.send(sum.toString());
+});
